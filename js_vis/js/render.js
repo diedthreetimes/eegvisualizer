2e03e5c6c5da9c0d0c5cf1fa7a5162cb70067b9c
Sky Faber <sky.faber@gmail.com>
2017-08-10 03:08:48 -0700

    Fix rendering to have some visible differentiation during the scan interval

--- js_vis/js/render.js
+++ js_vis/js/render.js
@@ -9,11 +9,6 @@ var canvas = document.querySelector("canvas"),
     MIN_VAL = -500.0
     ;
 
-// num_processed_bufs and num_recorded_bufs keep track of an event that may never happen
-// When a websocket frame is received before the last one has finished processing
-// The best longterm fix for this issue is to create a msg based websocket protocol
-//   (this will remove the need for partial (carry over) buffers)
-
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }).strength(0.5))
     .force("charge", d3.forceManyBody())
@@ -90,15 +85,6 @@ function redrawColors(colors){
 }
 
 function colorize(value){
-  // Probably want something like https://github.com/Enideo/jquery-colors
-  // For now we do simple things with hues
-  // Optimally lets work between two hues
-  //
-  // function percentageToHsl(percentage, hue0, hue1) {
-  //  var hue = (percentage * (hue1 - hue0)) + hue0;
-  //  return 'hsl(' + hue + ', 100%, 50%)';
-  //}
-  
   // Map value to a 0...1 scale
   var i = (value - MIN_VAL) / (MAX_VAL - MIN_VAL)
   
@@ -110,12 +96,9 @@ function colorize(value){
     i = 1.0;
   }
   
-  // convert decimal to hue
-  var hue = i * 1.2 / 360;
-  var rgb = hslToRgb(hue, 1, .5);
-  
-  // format to hex string and return
-  return 'rgb('+rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
+  var hue = (i * (120 - 0)) + 0;
+  //var hue = (i * (hue1 - hue0)) + hue0;
+  return 'hsl(' + hue + ', 100%, 50%)';
 }
 
 function redraw() {
@@ -134,42 +117,4 @@ function drawNode(d) {
 
   context.fillStyle = d.color;
   context.fill();
-}
-
-// Cdoe function from:
-//     https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   {number}  h       The hue
- * @param   {number}  s       The saturation
- * @param   {number}  l       The lightness
- * @return  {Array}           The RGB representation
- */
-function hslToRgb(h, s, l){
-    var r, g, b;
-
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
